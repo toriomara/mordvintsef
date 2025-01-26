a450585e37e6ce2cc7a399fa542e3aa0beb25318
Vladimir Budkevich <budkevich.vladimir@gmail.com>
2025-01-26 11:39:51 +0300

    feat: add publishable key

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en" suppressHydrationWarning>
         <body
           className={`${inter.variable} antialiased flex flex-col h-screen w-full`}
