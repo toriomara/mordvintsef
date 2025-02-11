a58a3813d9e5373f32c48c669370767360820033
Vladimir Budkevich <budkevich.vladimir@gmail.com>
2025-02-11 08:15:12 +0300

    fix: searchPage5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ import { useDebounce } from "use-debounce";
 import { Input } from "./ui/input";
 import { useEffect, useState } from "react";
 
-export function Search({ placeholder }) {
+export function Search() {
   const [text, setText] = useState("");
   const searchParams = useSearchParams("");
   const pathname = usePathname();
@@ -28,14 +28,14 @@ export function Search({ placeholder }) {
   };
 
   return (
-    <div className="relative flex justify-center space-y-4">
+    <div className="relative flex h-14 text-3xl text-primary items-center border-b">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <AiOutlineSearch className="relative self-center left-8 h-5 w-5 text-zinc-500 dark:text-zinc-100 peer-focus:text-zinc-500" />
       <Input
-        className="peer block w-1/2 rounded-md border py-[9px] pl-10 text-sm outline-2 placeholder:text-zinc-500"
-        placeholder={placeholder}
+        className="w-[92%] h-[100%] text-xl md:text-lg p-0 pl-10 bg-card border-none focus-visible:ring-0"
+        placeholder="Поиск..."
         type="search"
         onChange={handleInput}
         defaultValue={searchParams.get("query")?.toString()}
